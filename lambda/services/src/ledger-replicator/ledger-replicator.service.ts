05589f8d74e30688ce3d6dd52ef91113a98e4c47
Yathurshan Thurairajah <yathurshant@xeptagon.com>
2023-01-27 17:35:55 +0530

    changes in replicater file for updating coordinates

--- lambda/services/src/ledger-replicator/ledger-replicator.service.ts
+++ lambda/services/src/ledger-replicator/ledger-replicator.service.ts
@@ -25,7 +25,7 @@ export class LedgerReplicatorService {
     private configService: ConfigService
   ) {}
 
-  async forwardGeocoding(address: any) {
+  async forwardGeocoding(address: any[]) {
     let geoCodinates: any[] = [];
     const ACCESS_TOKEN =
       "";
