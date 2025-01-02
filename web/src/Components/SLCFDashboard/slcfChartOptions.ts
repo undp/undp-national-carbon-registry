e5c2e8c361421fe4f180551a2bd0b396aa511b6b
tharindugayanga <tharindug@xeptagon.com>
2025-01-02 16:26:56 +0530

    SLCR-227 SLCR-220 bug fixes

--- web/src/Components/SLCFDashboard/slcfChartOptions.ts
+++ web/src/Components/SLCFDashboard/slcfChartOptions.ts
@@ -70,15 +70,6 @@ export const totalProgrammesOptions: any = {
   },
   yaxis: {
     show: true,
-    // min: 0, // Set minimum value
-    // // max: function () {
-    // //   // Check if there's any data in the series
-    // //   const hasData = totalProgrammesOptions.series.some((s: any) => s.data.length > 0);
-    // //   return hasData ? undefined : 10; // Default max value if no data
-    // // },
-    // // eslint-disable-next-line no-use-before-define, @typescript-eslint/no-use-before-define
-    // max: totalProgrammesOptions.series.length ? undefined : 10, // Set default range if no data
-    // forceNiceScale: true, // Ensure scale looks good
     axisBorder: {
       show: true, // Enable the Y-axis line
       width: 1, // Thickness of the axis line
@@ -121,10 +112,10 @@ export const totalProgrammesOptions: any = {
       'rgba(72, 150, 254, 1)',
       'rgba(255, 99, 97, 1)',
       'rgba(72, 150, 254, 1)',
-      'rgba(255, 99, 97, 1)',
       'rgba(72, 150, 254, 1)',
       'rgba(72, 150, 254, 1)',
-      'rgba(255, 99, 97, 1)',
+      'rgba(72, 150, 254, 1)',
+      'rgba(72, 150, 254, 1)',
       'rgba(22, 200, 199, 1)',
     ],
   },
@@ -178,10 +169,10 @@ export const totalProgrammesOptions: any = {
         'rgba(72, 150, 254, 1)',
         'rgba(255, 99, 97, 1)',
         'rgba(72, 150, 254, 1)',
-        'rgba(255, 99, 97, 1)',
         'rgba(72, 150, 254, 1)',
         'rgba(72, 150, 254, 1)',
-        'rgba(255, 99, 97, 1)',
+        'rgba(72, 150, 254, 1)',
+        'rgba(72, 150, 254, 1)',
         'rgba(22, 200, 199, 1)',
       ],
       radius: 12,
@@ -228,24 +219,14 @@ export const retirementsByDateOptions: any = {
   },
   dataLabels: {
     enabled: true,
-    // offsetY: -20, // Adjust to position the label above the bar
     style: {
       fontSize: '12px',
-      colors: ['#000'], // Optional: Customize label color
+      colors: ['#000'],
       fontWeight: 500,
     },
-    // formatter: function (val: any, opts: any) {
-    //   const { seriesIndex, dataPointIndex } = opts;
-    //   const seriesTotals = opts.w.globals.stackedSeriesTotals;
-    //   if (seriesIndex === opts.w.globals.series.length) {
-    //     // Only show the total on the last series
-    //     return `${seriesTotals[dataPointIndex]} (Total)`;
-    //   }
-    //   return val;
-    // },
   },
   annotations: {
-    points: [], // To dynamically populate with totals
+    points: [],
   },
   chart: {
     type: 'bar',
@@ -365,164 +346,6 @@ export const retirementsByDateOptions: any = {
   },
 };
 
-// export const totalProgrammesOptionsSub: any = {
-//   states: {
-//     normal: {
-//       filter: {
-//         type: 'none',
-//         value: 0,
-//       },
-//     },
-//     hover: {
-//       filter: {
-//         type: 'none',
-//         value: 0,
-//       },
-//     },
-//     active: {
-//       allowMultipleDataPointsSelection: true,
-//       filter: {
-//         type: 'darken',
-//         value: 0.7,
-//       },
-//     },
-//   },
-//   dataLabels: {
-//     enabled: false,
-//   },
-//   chart: {
-//     type: 'bar',
-//     height: 400,
-//     stacked: true,
-//     stackType: 'normal',
-//   },
-//   responsive: [
-//     {
-//       breakpoint: 480,
-//       options: {
-//         legend: {
-//           position: 'bottom',
-//           offsetX: -10,
-//           offsetY: 0,
-//         },
-//       },
-//     },
-//   ],
-//   xaxis: {
-//     categories: [],
-//     labels: {
-//       rotatealways: true,
-//     },
-//   },
-//   yaxis: {
-//     show: true,
-//     title: {
-//       text: '',
-//       rotate: -90,
-//       offsetX: 0,
-//       offsetY: 0,
-//       style: {
-//         color: '#263238',
-//         fontSize: '12px',
-//         fontFamily: 'Inter',
-//         fontWeight: 500,
-//         cssClass: 'apexcharts-yaxis-title',
-//       },
-//     },
-//     labels: {
-//       formatter: (value: any) => {
-//         return addCommSepRound(value);
-//       },
-//     },
-//   },
-//   fill: {
-//     opacity: 1,
-//     colors: [
-//       '#16B1FF',
-//       '#FF8183',
-//       '#7FEABF',
-//       '#FFB480',
-//       '#666699',
-//       '#009999',
-//       '#c2c2d6',
-//       '#ddddbb',
-//       '#ff9900',
-//       '#D8D8D8',
-//     ],
-//   },
-//   title: {
-//     text: '',
-//     align: 'left',
-//     margin: 10,
-//     offsetX: 0,
-//     offsetY: 0,
-//     floating: false,
-//     style: {
-//       fontSize: '16px',
-//       fontWeight: 'bold',
-//       fontFamily: 'Inter',
-//       color: '#263238',
-//     },
-//   },
-//   legend: {
-//     show: true,
-//     showForSingleSeries: false,
-//     showForNullSeries: false,
-//     showForZeroSeries: false,
-//     position: 'bottom',
-//     horizontalAlign: 'center',
-//     floating: false,
-//     fontSize: '14px',
-//     fontFamily: 'Inter',
-//     fontWeight: 400,
-//     formatter: undefined,
-//     inverseOrder: false,
-//     width: undefined,
-//     height: undefined,
-//     tooltipHoverFormatter: undefined,
-//     customLegendItems: [],
-//     offsetX: 0,
-//     offsetY: 5,
-//     labels: {
-//       colors: '#000000d9',
-//       useSeriesColors: false,
-//     },
-//     markers: {
-//       width: 12,
-//       height: 12,
-//       strokeWidth: 0,
-//       strokeColor: '#fff',
-//       fillColors: [
-//         '#16B1FF',
-//         '#FF8183',
-//         '#7FEABF',
-//         '#FFB480',
-//         '#666699',
-//         '#009999',
-//         '#c2c2d6',
-//         '#ddddbb',
-//         '#ff9900',
-//         '#D8D8D8',
-//       ],
-//       radius: 12,
-//       customHTML: undefined,
-//       onClick: undefined,
-//       offsetX: 0,
-//       offsetY: 0,
-//     },
-//     itemMargin: {
-//       horizontal: 5,
-//       vertical: 0,
-//     },
-//     onItemClick: {
-//       toggleDataSeries: true,
-//     },
-//     onItemHover: {
-//       highlightDataSeries: true,
-//     },
-//   },
-// };
-
 //MARK: Credits by Date
 export const creditsByDateOptions: any = {
   states: {
@@ -550,19 +373,19 @@ export const creditsByDateOptions: any = {
     enabled: false,
     formatter: function (val: any) {
       if (val >= 1000) {
-        return `${(val / 1000).toFixed(1)}k`; // Convert to 'k' format for values >= 1000
+        return `${(val / 1000).toFixed(1)}k`;
       }
-      return val; // For smaller values, show the original number
+      return val;
     },
     style: {
       fontSize: '10px',
       fontFamily: 'Inter',
       fontWeight: 400,
-      colors: ['#000000d9'], // Customize label color
+      colors: ['#000000d9'],
     },
   },
   annotations: {
-    points: [], // To dynamically populate with totals
+    points: [],
   },
   chart: {
     type: 'bar',
@@ -570,7 +393,7 @@ export const creditsByDateOptions: any = {
     stacked: true,
     stackType: 'normal',
     toolbar: {
-      show: false, // Disables the toolbar, removing zoom, pan, and home icons
+      show: false,
     },
   },
   responsive: [
@@ -591,8 +414,8 @@ export const creditsByDateOptions: any = {
   yaxis: {
     show: true,
     axisBorder: {
-      show: true, // Enable the Y-axis line
-      width: 1, // Thickness of the axis line
+      show: true,
+      width: 1,
     },
     title: {
       text: '',
