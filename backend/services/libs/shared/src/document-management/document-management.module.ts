0341329d84a253032ec615de2a18997ebf9d36bb
Supun-D <supund@xeptagon.com>
2025-03-11 11:33:17 +0530

    Validation report creation

--- backend/services/libs/shared/src/document-management/document-management.module.ts
+++ backend/services/libs/shared/src/document-management/document-management.module.ts
@@ -4,12 +4,18 @@ import { UtilModule } from "../util/util.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { DocumentEntity } from "../entities/document.entity";
 import { ProgrammeLedgerModule } from "../programme-ledger/programme-ledger.module";
+import { AuditLogsModule } from "../audit-logs/audit-logs.module";
+import { EmailHelperModule } from "../email-helper/email-helper.module";
+import { FileHandlerModule } from "../file-handler/filehandler.module";
 
 @Module({
   imports: [
     UtilModule,
     TypeOrmModule.forFeature([DocumentEntity]),
     ProgrammeLedgerModule,
+    AuditLogsModule,
+    EmailHelperModule,
+    FileHandlerModule,
   ],
   providers: [DocumentManagementService],
   exports: [DocumentManagementService],
