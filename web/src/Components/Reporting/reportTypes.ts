619a527acae391e827df1463e35ef4019d205ddc
Ravindu Fernando <ravinduf@xeptagon.com>
2025-04-20 17:49:43 +0530

    Revert "Merge branch 'HG-289' of https://github.com/xeptagondev/carbon-registry-zim into add-sectoral-scope"
    This reverts commit fab099e9fb846384cbc49fbf0ea945f25cadd185, reversing
    changes made to b4c00b8b9323db761d5c90779ac5976ee4b19a6e.

--- web/src/Components/Reporting/reportTypes.ts
+++
@@ -1,4 +0,0 @@
-export enum REPORT_TYPES  {
-    ACTIONS = "ACTIONS",
-    HOLDINGS = "HOLDINGS",
-  };
--- web/src/Pages/Reports/Reports.tsx
+++ web/src/Pages/Reports/Reports.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import ReportingComponent from "../../Components/Reporting/ReportingComponent";
 
 const Reports = () => {
-  const { i18n } = useTranslation(["common", "reporting"]);
+  const { i18n } = useTranslation(["common"]);
 
   return <ReportingComponent translator={i18n} />
 };
