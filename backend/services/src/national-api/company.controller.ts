da05f9e85ffb354689bae776a6172ae480c21b1d
Lochana Oshadha <lochanaf@xeptagon.com>
2025-04-28 10:58:22 +0530

    show credit block balance in views company details view user details view

--- backend/services/src/national-api/company.controller.ts
+++ backend/services/src/national-api/company.controller.ts
@@ -178,7 +178,7 @@ export class CompanyController {
   @UseGuards(JwtAuthGuard)
   @Get("profile")
   async getCompany(@Query("id") companyId: number, @Request() req) {
-    return await this.companyService.findByCompanyId(companyId);
+    return await this.companyService.findByCompanyId(companyId, true);
   }
 
   @ApiBearerAuth()
