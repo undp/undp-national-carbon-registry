5b5f3cc47d7a821900935df47a36b88937e0cd83
gayanath8 <gayanathr@xeptagon.com>
2025-05-02 16:27:48 +0530

    Bug Fixes Dashboard - Status by detail widget legend missing field fix

--- web/src/Components/SLCFDashboard/slcfDetailsBarChartStatsComponent.tsx
+++ web/src/Components/SLCFDashboard/slcfDetailsBarChartStatsComponent.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react';
-import { Skeleton, Tooltip } from 'antd';
-import { InfoCircle } from 'react-bootstrap-icons';
+import React, { FC } from "react";
+import { Skeleton, Tooltip } from "antd";
+import { InfoCircle } from "react-bootstrap-icons";
 
 export interface DetailsBarChartStatsProps {
   id: string;
@@ -14,16 +14,31 @@ export interface DetailsBarChartStatsProps {
   width: string;
 }
 
-export const SLCFDetailsBarChartsStatComponent: FC<DetailsBarChartStatsProps> = (
-  props: DetailsBarChartStatsProps
-) => {
-  const { id, title, options, series, loading, toolTipText, Chart, height, width } = props;
+export const SLCFDetailsBarChartsStatComponent: FC<
+  DetailsBarChartStatsProps
+> = (props: DetailsBarChartStatsProps) => {
+  const {
+    id,
+    title,
+    options,
+    series,
+    loading,
+    toolTipText,
+    Chart,
+    height,
+    width,
+  } = props;
   return (
     <div className="statistics-and-pie-card height-bar-rem project-details-bar-chart">
       <div className="pie-charts-top">
         <div className="pie-charts-title">{title}</div>
         <div className="info-container">
-          <Tooltip arrowPointAtCenter placement="bottomRight" trigger="hover" title={toolTipText}>
+          <Tooltip
+            arrowPointAtCenter
+            placement="bottomRight"
+            trigger="hover"
+            title={toolTipText}
+          >
             <InfoCircle color="#000000" size={17} />
           </Tooltip>
         </div>
@@ -47,6 +62,7 @@ export const SLCFDetailsBarChartsStatComponent: FC<DetailsBarChartStatsProps> = 
             <div className="details-bar-chart-legends">
               <ul className="details-bar-chart-pending-list">
                 <li className="list-title">Pending</li>
+                <li className="list-item">Pending</li>
                 <li className="list-item">Approved</li>
                 <li className="list-item">PDD Submitted</li>
                 <li className="list-item">PDD Approved by Certifier</li>
@@ -55,6 +71,7 @@ export const SLCFDetailsBarChartsStatComponent: FC<DetailsBarChartStatsProps> = 
               </ul>
               <ul className="details-bar-chart-rejected-list">
                 <li className="list-title">Rejected</li>
+                <li className="list-item">Rejected</li>
                 <li className="list-item">PDD Rejected by Certifier </li>
                 <li className="list-item">PDD Rejected by DNA</li>
                 <li className="list-item">Validation Report Rejected</li>
