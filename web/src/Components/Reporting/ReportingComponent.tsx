2a8d4bee065765efd3daf0fdfe25744911c93e93
Amila Dissanayake <amilad@xeptagon.com>
2025-05-06 14:32:15 +0530

    aef query update

--- web/src/Components/Reporting/ReportingComponent.tsx
+++ web/src/Components/Reporting/ReportingComponent.tsx
@@ -250,6 +250,9 @@ const ReportingComponent = (props: { translator: i18n }) => {
       setHoldingsLoading(false);
     }
   };
+  const disabledDate = (current: any) => {
+    return current && current.year() < 1970;
+  };
   return (
     <div className="reporting-container">
       <div className="title-container">
@@ -262,6 +265,7 @@ const ReportingComponent = (props: { translator: i18n }) => {
             picker="year"
             allowClear={false}
             value={selectedYear}
+            disabledDate={disabledDate}
             onChange={(value: any) => {
               console.log("---------value-----------", value);
               const date = moment(value).local();
