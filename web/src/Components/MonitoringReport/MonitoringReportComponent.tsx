09a3664b6780718e24751912269a6281b059c4e5
Ravindu Fernando <ravinduf@xeptagon.com>
2025-06-20 09:28:27 +0530

    refactoring

--- web/src/Components/MonitoringReport/MonitoringReportComponent.tsx
+++ web/src/Components/MonitoringReport/MonitoringReportComponent.tsx
@@ -27,28 +27,11 @@ export const MonitoringReportComponent = (props: { translator: i18n }) => {
     setSelectedVersion(value);
   };
 
-  // const getDocVersions = async () => {
-  //   try {
-  //     const { data } = await post(API_PATHS.VERIFICATION_DOC_VERSIONS, {
-  //       programmeId: id,
-  //       verificationRequestId: Number(verificationRequestId),
-  //       docType: DocumentTypeEnum.MONITORING_REPORT,
-  //     });
-  //     setVersions(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+
 
   const handleDocumentStatus = (value: string) => {
     setDocumentStatus(value);
   };
-
-  // useEffect(() => {
-  //   if (mode === FormMode.VIEW || mode === FormMode.EDIT) {
-  //     getDocVersions();
-  //   }
-  // }, []);
 
   useEffect(() => {
     if (versions.length > 0) {
@@ -56,18 +39,7 @@ export const MonitoringReportComponent = (props: { translator: i18n }) => {
     }
   }, [versions]);
 
-  // const getCountryList = async () => {
-  //   const response = await get(API_PATHS.COUNTRY_LIST);
-  //   if (response.data) {
-  //     const alpha2Names = response.data.map((item: any) => {
-  //       return item.alpha2;
-  //     });
-  //     setCountries(alpha2Names);
-  //   }
-  // };
-  // useEffect(() => {
-  //   getCountryList();
-  // }, []);
+
   return (
     <div className="add-programme-main-container">
       <div className="title-container">
